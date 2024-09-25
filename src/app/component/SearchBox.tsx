0263ec554d9e6f09637a0572b06bb07f811fd6e3
PieMenard <pinedo.m.gustavo@gmail.com>
2024-09-24 21:21:38 -0400

    add debouncing to search

--- src/app/component/SearchBox.tsx
+++ src/app/component/SearchBox.tsx
@@ -1,17 +1,25 @@
+import { useDebouncedCallback } from 'use-debounce';
+
 type SearchBoxProps = {
   query: string;
   setQuery: (term: string) => void;
 };
 
 const SearchBox = ({ query, setQuery }: SearchBoxProps) => {
+  const handleChange = useDebouncedCallback(
+    (e: React.ChangeEvent<HTMLInputElement>) => {
+      setQuery(e.target.value);
+      console.log(query);
+    },
+    300
+  );
   return (
     <div>
       <input
         className="text-slate-900 px-2 mb-3 rounded-sm"
         placeholder="Search..."
         type="text"
-        value={query}
-        onChange={(e) => setQuery(e.target.value)}
+        onChange={handleChange}
       />
     </div>
   );
